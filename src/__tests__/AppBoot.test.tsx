622445a4c8742d80772919bbdbeeebf4d7b839f8
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-07-21 15:24:11 +0200

    test: Stub involved API request

--- src/__tests__/AppBoot.test.tsx
+++ src/__tests__/AppBoot.test.tsx
@@ -2,6 +2,21 @@ import { App } from '../App';
 
 describe('Booting the app', () => {
     it('successfully boots the app', () => {
+        cy.intercept('GET', 'http://localhost:3000/riddles', {
+            body: [
+                {
+                    id: 'X',
+                    contents: 'y',
+                    answers: [
+                        {
+                            id: 'z',
+                            text: 'ZZ',
+                        },
+                    ],
+                },
+            ],
+        });
+
         cy.mount(<App />, '/');
 
         cy.get('img[alt="Aero Logic"]').should('be.visible');
